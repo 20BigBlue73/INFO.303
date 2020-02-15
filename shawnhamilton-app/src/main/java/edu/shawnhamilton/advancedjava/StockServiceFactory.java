db21d3be933cb0b380ba0b5b78a05c551728f6ab
Shawn Hamilton <55065403+20BigBlue73@users.noreply.github.com>
2020-02-14 23:46:44 -0500

    another git commit

---
+++ shawnhamilton-app/src/main/java/edu/shawnhamilton/advancedjava/StockServiceFactory.java
@@ -0,0 +1,14 @@
+package edu.shawnhamilton.advancedjava;
+
+/*
+ * returns a concrete implementation of the StockService Interface 
+ */
+
+public class StockServiceFactory {
+	
+    public static StockService getStockService() {
+    	
+    	return new BasicStockService();
+    }
+    
+}
